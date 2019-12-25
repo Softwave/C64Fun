165772dcd23a1fc5cf950eaa2501de07008a8cad
JC Leyba <jcleyba92@gmail.com>
2019-12-24 17:49:57 -0700

    Changed label in flash program
    Was using a junk label - made it a descriptive one

--- flash/flash.asm
+++ flash/flash.asm
@@ -8,10 +8,10 @@ BasicUpstart2(start)
 
 start:
 	ldx #$00 // Load 0 into x 
-gloop:
+flash:
 	stx $d020 // Store x in d020, setting color
 	inx // Increment x 
 	cpx #$0f // Compare x to 15
-	bne gloop // Loop back up if we're not at 15
+	bne flash // Loop back up if we're not at 15
 	ldx #$00 // If we are at 15, reset x to 0 (black)
-	jmp gloop // Now we can go back up
+	jmp flash // Now we can go back up
