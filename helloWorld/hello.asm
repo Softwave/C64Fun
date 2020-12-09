a42d2ad830a448b898c389a456b40fe486cb0b85
Jessica Leyba <development@softwavestudios.com>
2020-12-09 16:26:33 -0700

    Decided to use clearer integer values for this example

--- helloWorld/hello.asm
+++ helloWorld/hello.asm
@@ -4,10 +4,10 @@ BasicUpstart2(start) // 10 sys 16384
 * = $4000 // Mem location
 
 start:
-	lda #$00  // Load 0 for black
+	lda #0  // Load 0 for black
 	sta $d020 // Make border black
 	sta $d021 // Make center of screen black
-	lda #$01 // Load 1 for white
+	lda #1 // Load 1 for white
 	sta $0286 // Make text white 
 	jsr $e544 // Clear the screen 
 	jsr draw_text // Jump to the draw_text sub
@@ -18,11 +18,11 @@ msg:
 	.text "              hello world!              " 
 
 draw_text:
-	ldx #$00
+	ldx #0
 draw_loop:
 	lda msg,x // Load the memory location of msg, plus x, into a 
 	sta $05e0,x // Put a into memory location 05e0 + x to show on screen
 	inx // Increment x, this is our index 
-	cpx #$28 // Compare x to 40 (our message is 40 chars long)
+	cpx #40 // Compare x to 40 (our message is 40 chars long)
 	bne draw_loop // If it's not, go back to draw loop, otherwise continue
 	rts // Return to sub
